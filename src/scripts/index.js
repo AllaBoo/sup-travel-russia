8b35cdcc2c15cf2dc3629551883059b7d581bc81
AllaBoo <a.v.buteeva@gmail.com>
2021-02-21 17:37:19 +0300

    добавлена анимация ккартинок в меню услуг

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 import '../styles/index.css';
 import { Header } from './Header';
+import Cursor from './cursor';
+import {preloader} from './preloader';
+import LocomotiveScroll from 'locomotive-scroll';
+import Menu from './menu';
 
 const headerIcon = document.querySelector('.header__icon');
 const leadButton = document.querySelector('#leadButton');
@@ -16,4 +20,19 @@ leadButton.addEventListener('click', () => {
 
 closeButton.addEventListener('click', () => {
   leadPopup.closest('.popup').classList.remove('popup_opened');
+});
+
+// menu (<nav> element)
+const menuEl = document.querySelector('.html');
+
+// preload the images set as data attrs in the menu items
+preloader('.menu__item').then(() => {
+    // initialize the smooth scroll
+    const scroll = new LocomotiveScroll({el: menuEl, smooth: true, scrollFromAnywhere: true, repeat: true});
+
+    // initialize custom cursor
+    // const cursor = new Cursor(document.querySelector('.cursor'));
+
+    // initialize menu
+    new Menu(menuEl);
 });
