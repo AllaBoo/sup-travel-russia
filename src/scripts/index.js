11a12d3a21819df40c60ee5291a8205eeb575e93
AllaBoo <a.v.buteeva@gmail.com>
2021-02-22 17:36:01 +0300

    добавлена медиа-галерея в начальном состоянии

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import '../styles/index.css';
 import { Header } from './Header';
-import Cursor from './cursor';
 import {preloader} from './preloader';
 import LocomotiveScroll from 'locomotive-scroll';
 import Menu from './menu';
@@ -23,15 +22,12 @@ closeButton.addEventListener('click', () => {
 });
 
 // menu (<nav> element)
-const menuEl = document.querySelector('.html');
+const menuEl = document.querySelector('#locomotiveScroll');
 
 // preload the images set as data attrs in the menu items
 preloader('.menu__item').then(() => {
     // initialize the smooth scroll
     const scroll = new LocomotiveScroll({el: menuEl, smooth: true, scrollFromAnywhere: true, repeat: true});
-
-    // initialize custom cursor
-    // const cursor = new Cursor(document.querySelector('.cursor'));
 
     // initialize menu
     new Menu(menuEl);
