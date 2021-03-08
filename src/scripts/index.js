6b7be2736eedbcda5c31657d2adf0e581406b27c
AllaBoo <a.v.buteeva@gmail.com>
2021-03-08 19:40:16 +0300

    добавлена фотогалерея

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,55 @@ import '../styles/index.css';
 import {preloader} from './preloader';
 import LocomotiveScroll from 'locomotive-scroll';
 import Menu from './menu';
-import { setHeaderListeners } from './constants';
+import { Popup } from './Popup';
+import { setHeaderListeners, photoPopup } from './constants';
 
 setHeaderListeners();
+
+const popupPhoto = new Popup(photoPopup);
+
+function photoZoomer (pic) {
+	const photo = pic.getAttribute('src');
+	document.querySelector('#prevPhoto').addEventListener('click', function() {
+		navigate('left');
+	});
+
+	document.querySelector('#nextPhoto').addEventListener('click', function() {
+		navigate('right');
+	});
+
+	document.querySelector('.popup__photo').setAttribute('src', photo);
+	popupPhoto.open();
+	
+	const items = document.querySelectorAll('.tiles__line-img');
+	let linksArr = []
+	items.forEach(element => {
+		linksArr.push(element.currentSrc);
+		return linksArr;
+	});
+	let current = linksArr.findIndex(item => item.includes(photo.slice(2)));
+	
+	document.querySelector('#last-photo').textContent = items.length > 10 ? items.length : `0${items.length}`;
+	document.querySelector('#cur-photo').textContent = current > 10 ? current+1  : `0${current+1}`;
+
+	const navigate = function(dir) {
+		if (dir === 'right') {
+			current = current < items.length-1 ? current + 1 : 0;
+			document.querySelector('.popup__photo').setAttribute('src', linksArr[current]);
+		} else {
+			current = current > 0 ? current - 1 : items.length-1;
+			document.querySelector('.popup__photo').setAttribute('src', linksArr[current]);
+		}
+		document.querySelector('#cur-photo').textContent = current > 10 ? current+1 : `0${current+1}`;
+	}
+	
+}
+
+document.addEventListener('click', (event) => {
+	if (event.target.classList.contains('tiles__line-img')) {
+		photoZoomer(event.target)
+	}
+})
 
 // menu (<nav> element)
 const menuEl = document.querySelector('#locomotiveScroll');
@@ -19,6 +65,7 @@ if (window.screen.availWidth > 768) {
 				new Menu(menuEl);
 		});
 }
+
 (function() {
 
 	function init(item) {
@@ -38,7 +85,7 @@ if (window.screen.availWidth > 768) {
     document.querySelector('.slider__progress').setAttribute('max', items.length);
 
 		items[current].classList.add("current");
-		var navigate = function(dir) {
+		const navigate = function(dir) {
 			items[current].classList.remove("current");
 
 			if (dir === 'right') {
--- src/scripts/program.js
+++ src/scripts/program.js
@@ -1,4 +1,51 @@
 import '../styles/program.css';
-import { setHeaderListeners } from './constants';
+import { Popup } from './Popup';
+import { setHeaderListeners, photoPopup } from './constants';
 
 setHeaderListeners();
+
+const popupPhoto = new Popup(photoPopup);
+
+function photoZoomer (pic) {
+	const photo = pic.getAttribute('src');
+	document.querySelector('#prevPhoto').addEventListener('click', function() {
+		navigate('left');
+	});
+
+	document.querySelector('#nextPhoto').addEventListener('click', function() {
+		navigate('right');
+	});
+
+	document.querySelector('.popup__photo').setAttribute('src', photo);
+	popupPhoto.open();
+	
+	const items = document.querySelectorAll('.tiles__line-img');
+	let linksArr = []
+	items.forEach(element => {
+		linksArr.push(element.currentSrc);
+		return linksArr;
+	});
+	let current = linksArr.findIndex(item => item.includes(photo.slice(2)));
+	
+	document.querySelector('#last-photo').textContent = items.length > 10 ? items.length : `0${items.length}`;
+	document.querySelector('#cur-photo').textContent = current > 10 ? current+1  : `0${current+1}`;
+
+	const navigate = function(dir) {
+		if (dir === 'right') {
+			current = current < items.length-1 ? current + 1 : 0;
+			document.querySelector('.popup__photo').setAttribute('src', linksArr[current]);
+		} else {
+			current = current > 0 ? current - 1 : items.length-1;
+			document.querySelector('.popup__photo').setAttribute('src', linksArr[current]);
+		}
+		document.querySelector('#cur-photo').textContent = current > 10 ? current+1 : `0${current+1}`;
+	}
+	
+}
+
+
+document.addEventListener('click', (event) => {
+	if (event.target.classList.contains('tiles__line-img')) {
+		photoZoomer(event.target)
+	}
+})
